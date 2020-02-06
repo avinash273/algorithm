03806e26162f1468c8e0f211fd39de3413f43c70
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-05 18:19:22 -0600

    linked list

---
+++ src/LinkedList/Node.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class Node {
+    private int data;
+    private  Node nextNode;
+
+    public  int getData(){
+        return this.data;
+    }
+
+    public Node getNextNode(){
+        return this.nextNode;
+    }
+
+    public void setData(int data){
+        this.data = data;
+    }
+
+    public void setNextNode(Node nextNode){
+        this.nextNode = nextNode;
+    }
+}
---
+++ src/LinkedList/linkedlist.java
@@ -0,0 +1,5 @@
+package LinkedList;
+
+public class linkedlist {
+    private Node head;
+}
