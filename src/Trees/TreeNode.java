7542ed8bf5d3f91ede2330bc1b6103e3ad1edc57
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-07 16:22:34 -0600

    Trees

--- src/Trees/TreeNode.java
+++ src/Trees/TreeNode.java
@@ -1,17 +1,61 @@
 package Trees;
 
 public class TreeNode {
-    private int data;
+    private Integer data;
     private TreeNode leftChild;
     private TreeNode rightChild;
-    private boolean isDeleted = false;
 
-    public TreeNode(int data) {
+    public TreeNode(Integer data) {
         this.data = data;
     }
 
-    public TreeNode find(int data) {
-        if (this.data == data && !isDeleted)
+    public static TreeNode addSorted(int[] data, int start, int end) {
+        if (end >= start) {
+            int mid = (start+end)/2;
+            TreeNode newNode = new TreeNode(data[mid]);
+            newNode.leftChild = addSorted(data, start, mid-1);
+            newNode.rightChild = addSorted(data, mid+1, end);
+            return newNode;
+        }
+        return null;
+    }
+
+    public int height() {
+        if (isLeaf()) return 1;
+        int left = 0;
+        int right = 0;
+        if (this.leftChild != null)
+            left = this.leftChild.height();
+        if (this.rightChild != null)
+            right = this.rightChild.height();
+        return (left > right) ? (left + 1) : (right + 1);
+    }
+
+    public int numOfLeafNodes() {
+        if (isLeaf()) return 1;
+        int leftLeaves = 0;
+        int rightLeaves = 0;
+        if (this.leftChild != null)
+            leftLeaves = leftChild.numOfLeafNodes();
+        if (this.rightChild != null)
+            rightLeaves = rightChild.numOfLeafNodes();
+        return leftLeaves + rightLeaves;
+    }
+
+    public boolean isLeaf() {
+        return this.leftChild == null && this.rightChild == null;
+    }
+
+    public void traverseInOrder() {
+        if (this.leftChild != null)
+            this.leftChild.traverseInOrder();
+        System.out.print(this + " ");
+        if (this.rightChild != null)
+            this.rightChild.traverseInOrder();
+    }
+
+    public TreeNode find(Integer data) {
+        if (this.data == data)
             return this;
         if (data < this.data && leftChild != null)
             return leftChild.find(data);
@@ -20,13 +64,13 @@ public class TreeNode {
         return null;
     }
 
-    public void insert(int data) {
-        if (data >= this.data) {
+    public void insert(Integer data) {
+        if (data >= this.data) { // insert in right subtree
             if (this.rightChild == null)
-                this.rightChild = new TreeNode((data));
+                this.rightChild = new TreeNode(data);
             else
                 this.rightChild.insert(data);
-        } else {
+        } else { // insert in left subtree
             if (this.leftChild == null)
                 this.leftChild = new TreeNode(data);
             else
@@ -34,7 +78,19 @@ public class TreeNode {
         }
     }
 
-    public int getData() {
+    public Integer largest() {
+        if (this.rightChild == null)
+            return this.data;
+        return this.rightChild.largest();
+    }
+
+    public Integer smallest() {
+        if (this.leftChild == null)
+            return this.data;
+        return this.leftChild.smallest();
+    }
+
+    public Integer getData() {
         return data;
     }
 
@@ -42,24 +98,20 @@ public class TreeNode {
         return leftChild;
     }
 
+    public void setLeftChild(TreeNode left) {
+        this.leftChild = left;
+    }
+
     public TreeNode getRightChild() {
         return rightChild;
     }
 
-    public void setLeftChild(TreeNode leftChild) {
-        this.leftChild = leftChild;
+    public void setRightChild(TreeNode right) {
+        this.rightChild = right;
     }
 
-    public void setRightChild(TreeNode rightChild) {
-        this.rightChild = rightChild;
+    @Override
+    public String toString() {
+        return String.valueOf(this.data);
     }
-
-    public void delete(){
-        this.isDeleted = true;
-    }
-
-    public boolean isDeleted(){
-        return isDeleted;
-    }
-
 }
---
+++ src/Trees/bstMain.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class bstMain {
+    public static void main(String[] args) {
+        int[] sample = { 212, 580, 6, 7, 28, 84, 112, 434};
+        BinarySearchTree bst = new BinarySearchTree();
+        for (int x : sample) {
+            bst.insert(x);
+        }
+        System.out.println(bst.find(65));
+        System.out.println(bst.smallest());
+        System.out.println(bst.largest());
+//		bst.delete(84);
+        System.out.println(bst.numOfLeafNodes());
+        System.out.println(bst.height());
+        bst.traverseInOrder();
+    }
+}
