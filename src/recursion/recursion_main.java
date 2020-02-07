911451094d90cd43abc05b3f32f282f008842bf3
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-06 18:35:24 -0600

    mergesort

--- src/recursion/recursion_main.java
+++ src/recursion/recursion_main.java
@@ -1,10 +1,18 @@
 package recursion;
 
+import java.util.Arrays;
+
 public class recursion_main {
     public static void main(String[] args){
         GCD GCD = new GCD();
         System.out.println(GCD.getGCD(30,250));
         factorial factorial = new factorial();
         System.out.println(factorial.getFactorial(10));
+
+        mergesort sorter = new mergesort();
+        int[] data = {4,1,6,9,5};
+        sorter.sort(data);
+        System.out.println(Arrays.toString(data));
+
     }
 }
