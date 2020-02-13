cc207a3a709f3c0be39031f891873ee40dd84035
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-12 19:26:11 -0600

    ArrayList and LinkedLists

---
+++ src/AdvancedJava/LinkedLists_ArrayList.java
@@ -0,0 +1,20 @@
+package AdvancedJava;
+
+import java.util.*;
+
+public class LinkedLists_ArrayList {
+    public static void main(String[] args){
+        //Linked List is better when you want to add more elements in between
+        //Linked List uses more space
+        //ArrayList used less space and
+        LinkedList<String> myList = new LinkedList();
+        myList.add("A");
+        myList.add("B");
+        myList.add(1, "C");
+        System.out.println(myList);
+        ArrayList myArr = new ArrayList();
+        myArr.add("A");
+        myArr.add(0,"B");
+        System.out.println(myArr);
+    }
+}
---
+++ src/tester.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+
+public class tester {
+    public static void main(String[] args){
+
+        System.out.println("\nTester Function to run random code!");
+        System.out.println("-----------------------------------");
+
+        int  max = Integer.MAX_VALUE+1;
+        System.out.println(max);
+
+        System.out.println("Testing varargs");
+
+
+/*        for (int i = 0; i <10 ; i++) {
+            for (int j = 0; j < i ; j++) {
+                for (int k = 0; k < i; k++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            //System.out.println("");
+        }*/
+    }
+}
