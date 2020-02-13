7b16a61bd09049f9c434337380fe623fb4aaecbd
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-12 20:30:40 -0600

    Using Interfaces

--- src/AdvancedJava/Hashmap.java
+++ src/AdvancedJava/Hashmap.java
@@ -1,11 +1,12 @@
 package AdvancedJava;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Hashmap {
     //No duplicates allowed
-    public static void main (String[] args){
-        HashMap<String, Integer> phonebook= new HashMap<>();
+    public static void main(String[] args) {
+        HashMap<String, Integer> phonebook = new HashMap<>();
         phonebook.put("Kevin", 2323423);
         phonebook.put("Jones", 346534);
         phonebook.put("Brenda", 123123);
@@ -16,6 +17,17 @@ public class Hashmap {
             System.out.println(phonebook.get("Brenda"));
 
         System.out.println(phonebook);
+
+        //LinkedHashMap is smart, it print most accessed at the bottom and keep track of order of entries
+        LinkedHashMap<String, Integer> phonebook2 = new LinkedHashMap<>(4, 0.75f, true);
+        phonebook2.put("Kevin", 2323423);
+        phonebook2.put("Jones", 346534);
+        phonebook2.put("Brenda", 123123);
+        phonebook2.put("Avi", 234234);
+        phonebook2.put("Jack", 574675);
+
+        System.out.println(phonebook2.get("Kevin"));
+        System.out.println(phonebook2);
 
     }
 }
---
+++ src/AdvancedJava/Main.java
@@ -0,0 +1,14 @@
+package AdvancedJava;
+
+public class Main {
+    public static void main(String[] args){
+        GreetingMessage gm = new GreetingMessage() {
+            @Override
+            public void greet(String name) {
+                System.out.println("Hello "+ name);
+            }
+        };
+
+        gm.greet("Avinash");
+    }
+}
