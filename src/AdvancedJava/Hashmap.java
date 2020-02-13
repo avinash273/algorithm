42283f1f4df4d5ce7aa40928b7453c7f09070869
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-12 20:02:34 -0600

    HashMaps

--- src/AdvancedJava/Hashmap.java
+++ src/AdvancedJava/Hashmap.java
@@ -3,6 +3,7 @@ package AdvancedJava;
 import java.util.HashMap;
 
 public class Hashmap {
+    //No duplicates allowed
     public static void main (String[] args){
         HashMap<String, Integer> phonebook= new HashMap<>();
         phonebook.put("Kevin", 2323423);
@@ -10,6 +11,9 @@ public class Hashmap {
         phonebook.put("Brenda", 123123);
         phonebook.put("Avi", 234234);
         phonebook.put("Jack", 574675);
+
+        if (phonebook.containsKey("Brenda"))
+            System.out.println(phonebook.get("Brenda"));
 
         System.out.println(phonebook);
 
